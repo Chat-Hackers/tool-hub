e61a48c450090c0dceb8e6a5baa1fb6abf5b419f
John Evans <38507954+King-Mob@users.noreply.github.com>
2026-07-03 16:35:10 +0100

    add a redirect

--- web/App.tsx
+++ web/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
-import Chat from "./Chat";
+import Dashboard from "./Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="chat" element={<Chat />} />
+        <Route path="chat" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <meta
+                http-equiv="refresh"
+                content="0; url=https://chat-hackers.github.io"
+              ></meta>
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
